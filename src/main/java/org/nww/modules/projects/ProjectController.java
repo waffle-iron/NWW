b5fe25bc502fc79851ca9ea086e793a3d73a0fae
mgatz <business@mircogatz.de>
2016-06-23 00:01:31 +0200

    #4 :added project participant selection for creating a project
    new projects could now be saved
    new projects could now add / remove participants
    project form is prepared for adding / removing suppliers

--- src/main/java/org/nww/modules/projects/ProjectController.java
+++ src/main/java/org/nww/modules/projects/ProjectController.java
@@ -9,9 +9,15 @@ import javax.annotation.Resource;
 import javax.validation.Valid;
 
 import org.nww.app.AbstractApplicationController;
+import org.nww.modules.projects.orm.Project;
 import org.nww.modules.projects.orm.ProjectManager;
 import org.nww.modules.projects.orm.ProjectParticipantData;
+import org.nww.modules.projects.orm.ProjectSupplierData;
 import org.nww.modules.users.orm.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -30,10 +36,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequestMapping(value = "/network/projects")
 public class ProjectController extends AbstractApplicationController {
 	
-	/**
-	 * 
-	 */
 	private static final String TEMPLATE_CREATE_PROJECT = "projects/createProject";
+	private static final String TEMPLATE_EDIT_PROJECT = "projects/editProject";
 	private static final String REDIRECT_AFTER_ADD_MODE_CREATE = "create";
 	private static final String REDIRECT_AFTER_ADD_MODE_EDIT = "edit";
 	
@@ -55,6 +59,13 @@ public class ProjectController extends AbstractApplicationController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String projectList(Model model) {
+		// workaround for sorting, paging will be implemented later
+		Pageable p = new PageRequest(0, 1000, Direction.DESC, "lastModified");
+		
+		Page<? extends Project> projects = projectMgr.findAll(p);
+		
+		model.addAttribute("Projects", projects.getContent());
+		
 		return "projects/projectList";
 	}
 	
@@ -73,15 +84,62 @@ public class ProjectController extends AbstractApplicationController {
 	}
 	
 	@RequestMapping(value = "/addParticipant.do", method = RequestMethod.POST)
-	public String addParticipantForCreation(
+	public String addParticipant(
 			@ModelAttribute("ProjectForm") ProjectForm form, 
 			@RequestParam(required = false, defaultValue = REDIRECT_AFTER_ADD_MODE_CREATE) String mode,
 			Model model) {
 		
 		form.getParticipants().add(0, new ProjectParticipantData());
 		
-		if(REDIRECT_AFTER_ADD_MODE_EDIT.equals(model)) {
-			return "";
+		if(REDIRECT_AFTER_ADD_MODE_EDIT.equals(mode)) {
+			return TEMPLATE_EDIT_PROJECT;
+		}
+		
+		return TEMPLATE_CREATE_PROJECT;
+	}
+	
+	@RequestMapping(value = "/removeParticipant.do", method = RequestMethod.POST)
+	public String removeParticipant(
+			@ModelAttribute("ProjectForm") ProjectForm form,
+			@RequestParam(required = true) Integer removeIndex,
+			@RequestParam(required = false, defaultValue = REDIRECT_AFTER_ADD_MODE_CREATE) String mode,
+			Model model) {
+		
+		form.getParticipants().remove((int)removeIndex);
+		
+		if(REDIRECT_AFTER_ADD_MODE_EDIT.equals(mode)) {
+			return TEMPLATE_EDIT_PROJECT;
+		}
+		
+		return TEMPLATE_CREATE_PROJECT;
+	}
+	
+	@RequestMapping(value = "/addSupplier.do", method = RequestMethod.POST)
+	public String addSupplier(
+			@ModelAttribute("ProejctForm") ProjectForm form,
+			@RequestParam(required = false, defaultValue = REDIRECT_AFTER_ADD_MODE_CREATE) String mode,
+			Model model) {
+		
+		form.getSuppliers().add(0, new ProjectSupplierData());
+		
+		if(REDIRECT_AFTER_ADD_MODE_EDIT.equals(mode)) {
+			return TEMPLATE_EDIT_PROJECT;
+		}
+		
+		return TEMPLATE_CREATE_PROJECT;
+	}
+	
+	@RequestMapping(value = "/removeSupplier.do", method = RequestMethod.POST)
+	public String removeSupplier(
+			@ModelAttribute("ProjectForm") ProjectForm form,
+			@RequestParam(required = true) Integer removeIndex,
+			@RequestParam(required = false, defaultValue = REDIRECT_AFTER_ADD_MODE_CREATE) String mode,
+			Model model) {
+		
+		form.getSuppliers().remove((int)removeIndex);
+		
+		if(REDIRECT_AFTER_ADD_MODE_EDIT.equals(mode)) {
+			return TEMPLATE_EDIT_PROJECT;
 		}
 		
 		return TEMPLATE_CREATE_PROJECT;
@@ -102,9 +160,10 @@ public class ProjectController extends AbstractApplicationController {
 			RedirectAttributes redirectAttributes, Model model) {
 		
 		if(!bindingResult.hasErrors()) {
-			boolean test = true;
-			//redirectAttributes.addAttribute("PCS", true);
-			//return "redirect:/network/projects/";
+			projectMgr.save(mapper.mapToPersistentObject(form));
+			
+			redirectAttributes.addAttribute("PCS", true);
+			return "redirect:/network/projects/";
 		}
 		
 		return TEMPLATE_CREATE_PROJECT;
