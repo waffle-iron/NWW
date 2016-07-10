cce8feba601bb13b0b86b43a770177fb13993172
mgatz <business@mircogatz.de>
2016-07-10 21:31:04 +0200

    fixed sample unit test

--- src/test/java/tests/nww/SampleTest.java
+++ src/test/java/tests/nww/SampleTest.java
@@ -9,21 +9,17 @@ import static org.junit.Assert.assertThat;
 import java.util.Locale;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.nww.NetzwerkWohnenApp;
 import org.nww.core.utils.DefaultLocaleHelper;
 import org.nww.core.utils.LocaleHelper;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
  *
  * @author mga
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = NetzwerkWohnenApp.class)
-@WebAppConfiguration
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration
+//@SpringApplicationConfiguration(classes = NetzwerkWohnenApp.class)
+//@WebAppConfiguration
 public class SampleTest {
 	
 	private LocaleHelper lh = new DefaultLocaleHelper();
